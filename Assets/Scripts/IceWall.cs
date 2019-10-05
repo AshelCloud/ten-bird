ac67cd4ef7c95ebe1aaa3a9330f3ff8d9145f2d2
AubeCiel <kk01045422490@gmail.com>
2019-10-05 13:11:36 +0900

    add Eventable object

---
+++ Assets/Scripts/IceWall.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IceWall : MonoBehaviour, IEventable
+{
+    public float meltTime = 3f;
+
+    public void ActiveEvent()
+    {
+    }
+
+    private IEnumerator Melt(float t)
+    {
+        yield return new WaitForSeconds(t);
+
+        ActiveEvent();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            StartCoroutine(Melt(meltTime));
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            StopAllCoroutines();
+        }
+    }
+}
