a77737e9e7758f1960dc877bc0bb74ab6993997c
AubeCiel <kk01045422490@gmail.com>
2019-10-05 23:13:06 +0900

    add joystick

--- Assets/Scripts/player.cs
+++ Assets/Scripts/player.cs
@@ -4,22 +4,35 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public float Spend = 1f;
     private Vector3 startPoint;
-
+    public float Spend = 1f;
+    public float rotationSpeed = 1f;
+    private Rigidbody rb;
+    private Vector3 m_CamForward;
     private void Awake()
     {
         startPoint = Vector3.zero;
+        rb = GetComponent<Rigidbody>();
+        m_CamForward = Vector3.Scale(Camera.main.transform.forward, new Vector3(1, 0, 1)).normalized;
     }
 
     void Start()
     {
-        startPoint = GameObject.Find("StartPoint").transform.position;
-        transform.position = startPoint;
+        //startPoint = GameObject.Find("StartPoint").transform.position;
+        //transform.position = startPoint;
     }
 
-    void Update()
+    void FixedUpdate()
     {
+        float h = UltimateJoystick.GetHorizontalAxis("Move");
+        float v = UltimateJoystick.GetVerticalAxis("Move");
+
+        m_CamForward = Vector3.Scale(Camera.main.transform.forward, new Vector3(1, 0, 1)).normalized;
+        Vector3 move = v * m_CamForward + h * Camera.main.transform.right;
+
+        rb.velocity = move;
+        
+        transform.Rotate(0f, h * rotationSpeed * Time.deltaTime, 0f);
     }
 }
 
