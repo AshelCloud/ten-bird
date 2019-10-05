cc20ceb679af714792261b8ed652d0874bcc82b4
AubeCiel <kk01045422490@gmail.com>
2019-10-05 15:11:05 +0900

    add Door

---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour, IEventable
+{
+    private Animator _anima;
+    public Animator Anima
+    {
+        get
+        {
+            if(_anima == null)
+            {
+                _anima = GetComponent<Animator>();
+            }
+            return _anima;
+        }
+    }
+
+    private void Start()
+    {
+        Anima.SetBool("Open", true);
+    }
+
+    public void ActiveEvent()
+    {
+    }
+}
