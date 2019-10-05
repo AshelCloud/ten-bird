d88120f03981a0f6f3d3a08ec0a50584f821a083
AubeCiel <kk01045422490@gmail.com>
2019-10-05 17:46:09 +0900

    add SoundManager

---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : Singleton<SoundManager>
+{
+    public AudioSource Audio { get; private set; }
+
+    private void Awake()
+    {
+        Audio = GetComponent<AudioSource>();
+
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public void PlayClip(AudioClip clip)
+    {
+        Audio.PlayOneShot(clip);
+    }
+}
